d6ae79dd661d43cb174f962c7bfccd447b9f695d
Cody Soyland <codysoyland@gmail.com>
2010-04-01 21:53:07 -0500

    Add support for "subsuites" and "suite.json" config files.
    - Scan directories to include links to suites.
    - Link to parent directory.
    - Support "suite.json" to name the suite and add additional javascript includes.
    - Rename 'run_tests.sh' to 'runserver.sh' and remove webbrowser loading (annoying).

--- django_qunit/urls.py
+++ django_qunit/urls.py
@@ -5,7 +5,6 @@ import os
 media_root = os.path.join(os.path.dirname(__file__), 'media')
 
 urlpatterns = patterns('',
-    url('^$', 'django_qunit.views.run_tests'),
     url(r'^tests/(?P<path>.*)$', 'django.views.static.serve', {
         'document_root': settings.QUNIT_TEST_DIRECTORY,
     }, name='qunit_test'),
@@ -15,4 +14,6 @@ urlpatterns = patterns('',
     url(r'^qunit/qunit.css', 'django.views.static.serve', {
         'document_root': media_root, 'path': 'qunit/qunit.css',
     }, name='qunit_css'),
+    url('^(?P<path>.*)$', 'django_qunit.views.run_tests',
+        name='qunit_test_overview'),
 )
--- example/run_tests.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-PYTHONPATH=`pwd`:`pwd`/..:$PYTHONPATH
-python -c "import webbrowser; webbrowser.open_new_tab('http://localhost:8080/qunit/')"
-django-admin.py runserver --settings=settings 8080 
