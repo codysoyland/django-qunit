3c208ba487db9c703be38a19fc5a0b736a6a92b8
Timothy Van Heest <timothy.vanheest@gtri.gatech.edu>
2013-06-24 15:39:14 -0400

    Moved snippet loader to base for easier packaging.  Cleaned up and tested setup.py.

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python
-from distutils.core import setup
+import os
+from setuptools import setup
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-qunit',
-    version='0.1.1',
+    version='20130624',
     description='QUnit Javascript testing integration for Django.',
-    long_description=open('README.md').read(),
-    author='Cody Soyland',
-    author_email='codysoyland@gmail.com',
-    url='http://github.com/codysoyland/django-qunit/',
+    long_description=README,
+    author='Timothy Van Heest',
+    author_email='timothy.vanheest@gmail.com',
+    url='http://github.com/turtlemonvh/django-qunit/',
     packages=[
         'django_qunit',
     ],
@@ -19,9 +25,11 @@ setup(
                  'Environment :: Web Environment',
                  'Framework :: Django',
                  'Intended Audience :: Developers',
-                 'License :: OSI Approved :: BSD License',
+                 'License :: OSI Approved :: MIT License',
                  'Operating System :: OS Independent',
                  'Programming Language :: Python',
+                 'Programming Language :: Python :: 2.6',
+                 'Programming Language :: Python :: 2.7',
                  'Programming Language :: JavaScript',
                  'Topic :: Software Development :: Testing'],
 )
